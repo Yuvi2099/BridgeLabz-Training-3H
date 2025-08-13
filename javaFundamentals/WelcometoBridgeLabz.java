a1d97cf06fd56f2d7236b925e0ec7590fabe32c0
Yuvraj <yuvraj.singh0317@gmail.com>
2025-08-13 15:12:48 +0530

    added successfully

---
+++ javaFundamentals/WelcometoBridgeLabz.java
@@ -0,0 +1,8 @@
+package javaFundamentals;
+import java.util.*;
+public class WelcometoBridgeLabz {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Bridgelabz!");
+    }
+
+}
