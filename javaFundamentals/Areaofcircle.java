4292be37bead15d9722476a89fba58370c248190
Yuvraj <yuvraj.singh0317@gmail.com>
2025-08-13 15:30:24 +0530

    added successfully

---
+++ javaFundamentals/Areaofcircle.java
@@ -0,0 +1,11 @@
+package javaFundamentals;
+import java.util.*;
+public class Areaofcircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of the circle: " + area);
+    }
+}
