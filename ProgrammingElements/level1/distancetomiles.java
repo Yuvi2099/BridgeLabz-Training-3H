edffbd0a4a45dfcef57baebd8d0beb909018fe7d
Yuvraj <yuvraj.singh0317@gmail.com>
2025-08-14 14:57:42 +0530

    file added

---
+++ ProgrammingElements/level1/distancetomiles.java
@@ -0,0 +1,12 @@
+package ProgrammingElements.level1;
+public class distancetomiles {
+    public static void main(String[] args) {
+        kmToMilesFixed();
+    }
+    public static void kmToMilesFixed() {
+        double km = 10.8;
+        double miles = km * 1.6;
+        System.out.println("The distance " + km + " km in miles is " + miles);
+    }
+
+}
