1ab0d26e92b59cc3b7e70b373a39fc0997090841
Yuvraj <yuvraj.singh0317@gmail.com>
2025-09-03 17:15:26 +0530

    [Yuvraj] ADD: added solution related to BOOK problem

---
+++ constructor/Books.java
@@ -0,0 +1,37 @@
+package constructor;
+
+class Book {
+    String title;
+    String author;
+    double price;
+
+    Book() {
+        title = "Unknown";
+        author = "Unknown";
+        price = 0.0;
+    }
+
+    Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    void displayDetails() {
+        System.out.println("Book Details:");
+        System.out.println("Title  : " + title);
+        System.out.println("Author : " + author);
+        System.out.println("Price  : " + price);
+    }
+}
+
+public class Books {
+    public static void main(String[] args) {
+        Book book1 = new Book();
+        book1.displayDetails();
+        System.out.println("------------------------");
+
+        Book book2 = new Book("1984", "George Orwell", 299.0);
+        book2.displayDetails();
+    }
+}
