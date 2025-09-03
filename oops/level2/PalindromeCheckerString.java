a67a3ce1f07859b9c3ae6f8d2e48aa1d3027146b
Yuvraj <yuvraj.singh0317@gmail.com>
2025-09-03 16:34:40 +0530

    [Yuvraj] ADD: added solution for the problem related to palindrome string

---
+++ oops/level2/PalindromeCheckerString.java
@@ -0,0 +1,35 @@
+package oops.level2;
+
+import java.util.;
+
+class PalindromeChecker {
+    String text;
+
+    PalindromeChecker(String text) {
+        this.text = text;
+    }
+
+    boolean isPalindrome() {
+        String reversed = new StringBuilder(text).reverse().toString();
+        return text.equalsIgnoreCase(reversed);
+    }
+
+    void displayResult() {
+        if (isPalindrome()) {
+            System.out.println("\"" + text + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + text + "\" is not a palindrome.");
+        }
+    }
+}
+
+public class PalindromeCheckerString {
+    public static void main(String[] args) {
+        PalindromeChecker pc1 = new PalindromeChecker("Madam");
+        PalindromeChecker pc2 = new PalindromeChecker("Hello");
+
+        pc1.displayResult();
+        pc2.displayResult();
+    }
+}
+
