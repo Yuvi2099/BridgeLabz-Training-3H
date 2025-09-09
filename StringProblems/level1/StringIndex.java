d9de4bdc82edc082bdb706182eab96844e94b05d
Yuvraj <yuvraj.singh0317@gmail.com>
2025-09-09 16:32:38 +0530

    [Yuvraj] ADD: added the solution for number string index

---
+++ StringProblems/level1/StringIndex.java
@@ -0,0 +1,29 @@
+package StringProblems.level1;
+
+import java.util.Scanner;
+
+public class StringIndex {
+
+    public static void GenerateException(String Input) {
+        char ch = Input.charAt(100);
+        System.out.println(ch);
+    }
+
+    public static void HandleException(String Input) {
+        try {
+            char ch = Input.charAt(100);
+            System.out.println(ch);
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Exception Handled");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner ScannerObj = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String UserInput = ScannerObj.nextLine();
+
+        // GenerateException(UserInput); 
+        HandleException(UserInput);
+    }
+}
