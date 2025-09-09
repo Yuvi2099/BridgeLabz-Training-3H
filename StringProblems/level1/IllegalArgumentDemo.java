21147638141aa98733a7ac3a2baaa3f265ec032d
Yuvraj <yuvraj.singh0317@gmail.com>
2025-09-09 16:23:49 +0530

    [Yuvraj] ADD: added the solution for illegal arguments

---
+++ StringProblems/level1/IllegalArgumentDemo.java
@@ -0,0 +1,31 @@
+package StringProblems.level1;
+import java.util.*;
+public class IllegalArgumentDemo {
+
+
+    public static void GenerateException(String Input) {
+        String Sub = Input.substring(5, 2);
+        System.out.println(Sub);
+    }
+
+    public static void HandleException(String Input) {
+        try {
+            String Sub = Input.substring(5, 2);
+            System.out.println(Sub);
+        } catch (IllegalArgumentException e) {
+            System.out.println("IllegalArgumentException Handled");
+        } catch (RuntimeException e) {
+            System.out.println("RuntimeException Handled");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner ScannerObj = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String UserInput = ScannerObj.nextLine();
+
+        // GenerateException(UserInput); 
+        HandleException(UserInput);
+    }
+}
+
