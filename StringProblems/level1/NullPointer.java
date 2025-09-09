faeb49438b6cb5b761ba85b2e951b917e0065a79
Yuvraj <yuvraj.singh0317@gmail.com>
2025-09-09 16:28:03 +0530

    [Yuvraj] ADD: added the solution for null pointer

---
+++ StringProblems/level1/NullPointer.java
@@ -0,0 +1,23 @@
+package StringProblems.level1;
+
+public class NullPointer {
+
+    public static void GenerateException() {
+        String Text = null;
+        Text.length();
+    }
+
+    public static void HandleException() {
+        String Text = null;
+        try {
+            Text.length();
+        } catch (NullPointerException e) {
+            System.out.println("Exception Handled");
+        }
+    }
+
+    public static void main(String[] args) {
+        // GenerateException();
+        HandleException();
+    }
+}
